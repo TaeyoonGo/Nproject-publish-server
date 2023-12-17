46a25fa69f27bfb084e33d3518780615a5c6330b
gotaeyoon <ty.go.payple.kr>
2023-12-18 08:45:27 +0900

    수정 작업 진행

--- payple-developer-v2/public/pagelist/data/data.js
+++ payple-developer-v2/public/pagelist/data/data.js
@@ -9,7 +9,7 @@ data.email = {
             page: '회원가입_인증'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '회원가입_완료',
             category: 'email',
             page: '회원가입_완료'
@@ -27,79 +27,79 @@ data.email = {
             page: '관리자_초대'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '상태변경_심사_서비스신청후',
             category: 'email',
             page: '상태변경_심사_서비스신청후'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '상태변경_심사_통과',
             category: 'email',
             page: '상태변경_심사_통과'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '상태변경_심사_일부통과',
             category: 'email',
             page: '상태변경_심사_일부통과'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '상태변경_심사_불가',
             category: 'email',
             page: '상태변경_심사_불가'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '상태변경_계약서발송',
             category: 'email',
             page: '상태변경_계약서발송'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '상태변경_계약완료',
             category: 'email',
             page: '상태변경_계약완료'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '이메일_민원',
             category: 'email',
             page: '이메일_민원'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '이메일_차지백',
             category: 'email',
             page: '이메일_차지백'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '이메일_공지',
             category: 'email',
             page: '이메일_공지'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '이메일_보증보험갱신_2주전',
             category: 'email',
             page: '이메일_보증보험갱신_2주전'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '이메일_보증보험갱신_3일전',
             category: 'email',
             page: '이메일_보증보험갱신_3일전'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '이메일_보증보험미갱신_서비스정지',
             category: 'email',
             page: '이메일_보증보험미갱신_서비스정지'
         },
         {
-            stateClass: 'confirm',
+            stateClass: 'waiting',
             url: '이메일_보증보험미갱신_정산보류',
             category: 'email',
             page: '이메일_보증보험미갱신_정산보류'
@@ -200,22 +200,16 @@ data.developercenter = {
             page: 'white_ip_10개이상등록'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '테스트결제내역_검색결과없음',
             category: 'developercenter',
             page: '테스트결제내역_검색결과없음'
         },
         {
-            stateClass: 'complete',
-            url: '테스트결제내역_조회결과_국내',
+            stateClass: 'confirm',
+            url: '테스트결제내역_조회결과_통합',
             category: 'developercenter',
-            page: '테스트결제내역_조회결과_국내'
-        },
-        {
-            stateClass: 'complete',
-            url: '테스트결제내역_조회결과_해외',
-            category: 'developercenter',
-            page: '테스트결제내역_조회결과_해외'
+            page: '테스트결제내역_조회결과_통합'
         },
     ]
 }
